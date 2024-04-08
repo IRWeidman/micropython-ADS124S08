c60cfc9354f0d27f0c0ac09c6dc12db7b1b7774f
Isaak Weidman <isaak.w@quub.space>
2024-04-08 14:14:22 -0400

    Added usage example

---
+++ examples/hwconfig.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""hwconfig.py
+
+This script is used to initialize any board-specific hardware. Make sure to
+edit all definitions to meet the requirements of your board, in this case a
+Raspberry Pi Pico.
+"""
+import machine
+
+ADS_RESET = machine.Pin(0)
+ADS_CS = machine.Pin(1)
+ADS_SCLK = machine.Pin(2)
+ADS_DIN = machine.Pin(3)
+ADS_DOUT = machine.Pin(4)
+ADS_SYNC = machine.Pin(5)
+ADS_DRDY = machine.Pin(6)
---
+++ examples/simple_scan.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""simple_scan.py
+
+Demonstrates the use of ads124s08.py by continuously printing each channel of
+the ADC unit to the screen.
+
+Use hwconfig.py to define the pins used in your specific scenario.
+"""
+import machine
+import time
+import hwconfig as hw
+import ads124s08
+
+# Create SPI object
+spi = machine.SPI(baudrate=10_000_000, polarity=0, phase=1,
+                  firstbit=machine.SPI.MSB, sck=hw.ADS_SCLK,
+                  mosi=hw.ADS_DIN, miso=hw.ADS_DOUT)
+# Create ads object
+ads = ads124s08.ADS124S08(spi=spi, cs=hw.ADS_CS, reset=hw.ADS_RESET,
+                          sync=hw.ADS_SYNC, drdy=hw.ADS_DRDY)
+
+while True:
+    # Wait for data availability
+    print('waiting for data', end='')
+    while not ads.data_ready:
+        print('.', end='')
+        time.sleep_ms(250)
+    print(ads.data_ready)
+
+    print('--------------------')
+
+    # Print the readings on each channel
+    for i in range(12):
+        ads.channel = i
+        reading = ads.read()
+
+        print(f'channel_{i:<4}|{reading:>7}')
+
+        time.sleep_ms(250)
+    print()
